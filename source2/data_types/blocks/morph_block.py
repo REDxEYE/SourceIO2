f53acd5284f2fca2f386c6203ca27fae09bd960b
REDxEYE <med45c@gmail.com>
2022-07-02 18:24:38 +0300

    Added shortcuts for few fields

--- source2/data_types/blocks/morph_block.py
+++ source2/data_types/blocks/morph_block.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from SourceIO2.source2.data_types.blocks import DataBlock, ResourceIntrospectionManifest
 from SourceIO2.source2.data_types.keyvalues3.binary_keyvalues import BinaryKeyValues
 from SourceIO2.source2.data_types.keyvalues3.enums import KV3Signatures
@@ -6,9 +8,13 @@ from SourceIO2.utils import IBuffer
 
 
 class MorphBlock(DataBlock):
+
+    def __init__(self, buffer: IBuffer, resource: ICompiledResource):
+        super().__init__(buffer, resource)
+
     @classmethod
-    def from_file(cls, buffer: IBuffer, resource: ICompiledResource) -> 'DataBlock':
-        self: 'DataBlock' = cls(buffer, resource)
+    def from_file(cls, buffer: IBuffer, resource: ICompiledResource) -> 'MorphBlock':
+        self: 'MorphBlock' = cls(buffer, resource)
         magic = buffer.read(4)
         buffer.seek(-4, 1)
         if KV3Signatures.is_valid(magic):
@@ -22,3 +28,19 @@ class MorphBlock(DataBlock):
         else:
             raise NotImplementedError('Unknown data block format')
         return self
+
+    @property
+    def lookup_type(self):
+        return self['m_nLookupType']
+
+    @property
+    def encoding_type(self):
+        return self['m_nEncodingType']
+
+    @property
+    def bundles(self) -> List[str]:
+        return self['m_bundleTypes']
+
+    def get_bundle_id(self, bundle_name):
+        if bundle_name in self.bundles:
+            return self.bundles.index(bundle_name)
