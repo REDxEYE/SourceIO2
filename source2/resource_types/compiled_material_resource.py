9f3805e0f8d20c731eced153057ac4e6b726608b
REDxEYE <med45c@gmail.com>
2022-07-03 18:06:51 +0300

    Added getters for material params

--- source2/resource_types/compiled_material_resource.py
+++ source2/resource_types/compiled_material_resource.py
@@ -1,3 +1,6 @@
+from functools import cache
+from typing import List, Dict
+
 from SourceIO2.source2 import CompiledGenericResource
 
 
@@ -7,5 +10,30 @@ class CompiledMaterialResource(CompiledGenericResource):
         used_textures = {}
         for texture in data_block['m_textureParams']:
             used_textures[texture['m_name']] = texture['m_pValue']
-
         return used_textures
+
+    @cache
+    def get_int_property(self, prop_name):
+        data, = self.get_data_block(block_name='DATA')
+        return self._get_prop(prop_name, data['m_intParams'], 'm_nValue')
+
+    @cache
+    def get_float_property(self, prop_name):
+        data, = self.get_data_block(block_name='DATA')
+        return self._get_prop(prop_name, data['m_intParams'], 'm_flValue')
+
+    @cache
+    def get_vector_property(self, prop_name):
+        data, = self.get_data_block(block_name='DATA')
+        return self._get_prop(prop_name, data['m_vectorParams'], 'm_value')
+
+    @cache
+    def get_texture_property(self, prop_name):
+        data, = self.get_data_block(block_name='DATA')
+        return self._get_prop(prop_name, data['m_textureParams'], 'm_pValue')
+
+    @staticmethod
+    def _get_prop(prop_name: str, prop_array: List[Dict], prop_value_name):
+        for prop in prop_array:
+            if prop['m_name'] == prop_name:
+                return prop[prop_value_name]
