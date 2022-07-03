156815f430ab18418fc0e8863d989419cdf8159b
REDxEYE <med45c@gmail.com>
2022-07-03 18:07:38 +0300

    Added source1/2 scale constants

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,7 @@
 from .file_utils import IBuffer, FileBuffer, MemoryBuffer, IFromFile
+
+# According to the Valve documentation,
+# one hammer unit is 1/16 of feet, and one feet is 30.48 cm
+SOURCE1_HAMMER_UNIT_TO_METERS = ((1 / 16) * 30.48) / 100
+# one hammer unit is 1/12 of feet, and one feet is 30.48 cm
+SOURCE2_HAMMER_UNIT_TO_METERS = ((1 / 12) * 30.48) / 100
